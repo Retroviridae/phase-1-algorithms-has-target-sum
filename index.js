fe6f85410353bbe156e7a85b8bc7161da08268b4
Alexander Schuessler <schuessler.alex@gmail.com>
2022-04-21 08:47:15 -0500

    commit tree 134d0c4bc08c70ac1dfb73a446cb777f310d02a4

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+const hasTargetSum = (array,target)=> {
+  for (let i = 0; i < array.length; i++){
+    const complement = target - array[i]
+    for (let j = i+1; j < array.length; j++){
+      // console.log(`Remainder: ${complement}`)
+      // console.log(`array[j]: ${array[j]}`)
+      if (array[j]=== complement) return true
+    }
+  }return false
 }
 
+
+
+console.log(hasTargetSum([4, 8, 12, 3, 11, 7], 10))
+console.log(hasTargetSum([4, 8, 12, 3, 11, 8], 10))
+console.log(hasTargetSum([5, 3, 8, 3, 1, 4], 15))
+console.log(hasTargetSum([5, 3, 8, 7, 1, 4], 15))
+// expect 3,7
 /* 
   Write the Big O time complexity of your function here
+  function hasTargetSum(array, target) {
+  // iterate over the array of numbers
+  for (let i = 0; i < array.length; i++) {
+    // for the current number, identify a complementary number that adds to our target
+    // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    const complement = target - array[i];
+    // iterate over the remaining numbers in the array
+    for (let j = i + 1; j < array.length; j++) {
+      // check if any of the remaining numbers is the complement
+      // if so, return true
+      if (array[j] === complement) return true;
+    }
+  }
+  // if we reach the end of the array, return false
+  return false;
+}
+
 */
 
 /* 
-  Add your pseudocode here
+  iterate over array [i]
+    iterate over array j=[i+1]
+      if arr[i]+arr[j]= solution{
+        return arri + arr j
+      }
 */
 
 /*
   Add written explanation of your solution here
-*/
+// */
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+// // You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+//   console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+//   console.log("Expecting: true");
+//   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+//   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
+//   console.log("Expecting: false");
+//   console.log("=>", hasTargetSum([1, 2, 5], 4));
+// }
 
-module.exports = hasTargetSum;
+// module.exports = hasTargetSum;
